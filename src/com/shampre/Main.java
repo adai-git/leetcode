8491d231bf73f1db3aa148607aee0f4f8db1b34f
DOU <806680796@qq.com>
2022-02-08 11:10:56 +0800

    字符串整数转化

--- src/com/shampre/Main.java
+++ src/com/shampre/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         String result = null;
-        String s="-91283472332";
+        String s="9223372036854775808";
         Solution solution = new Solution();
         result=String.valueOf(solution.myAtoi(s));
         System.out.println("result:\n"+result);
