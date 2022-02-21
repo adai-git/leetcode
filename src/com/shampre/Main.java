1e5eb58b95df70bec49d88160ac4a0b10fcfb1dc
DOU <806680796@qq.com>
2022-02-21 11:53:30 +0800

    正则表达式匹配

--- src/com/shampre/Main.java
+++ src/com/shampre/Main.java
@@ -1,14 +1,12 @@
 package com.shampre;
 
-import com.shampre.demo10.Solution;
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         String result = null;
         String s=s = "aab", p = "c*a*b";
-        Solution solution = new Solution();
+        Solution1 solution = new Solution1();
         result=String.valueOf(solution.isMatch(s,p));
         System.out.println("result:\n"+result);
     }
--- src/com/shampre/demo10/Solution.java
+++ src/com/shampre/demo10/Solution.java
@@ -1,51 +1,46 @@
 package com.shampre.demo10;
 
 /**
- * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
- * <p>
- * '.' 匹配任意单个字符
- * '*' 匹配零个或多个前面的那一个元素
- * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
- * <p>
- *  
- * 示例 1：
- * <p>
- * 输入：s = "aa" p = "a"
- * 输出：false
- * 解释："a" 无法匹配 "aa" 整个字符串。
+ * 正则表达式匹配
+ * 方法一：动态规划
+ * 思路与算法
+ *
+ * 题目中的匹配是一个「逐步匹配」的过程：我们每次从字符串 pp 中取出一个字符或者「字符 + 星号」的组合，
+ * 并在 ss 中进行匹配。对于 pp 中一个字符而言，它只能在 ss 中匹配一个字符，匹配的方法具有唯一性；而
+ * 对于 pp 中字符 + 星号的组合而言，它可以在 ss 中匹配任意自然数个字符，并不具有唯一性。因此我们可以
+ * 考虑使用动态规划，对匹配的方案进行枚举。
  */
 public class Solution {
     public boolean isMatch(String s, String p) {
-        char[] charArrS = s.toCharArray();
-        char[] charArrP = p.toCharArray();
-        int indexS = 0;
-        int indexP = 0;
-        boolean hasCompliant = true;
-        while (hasCompliant) {
-            char charS = charArrS[indexS];
-            char charP = charArrP[indexP];
-            if (charS == charP) {
-                indexS++;
-                indexP++;
-            } else if (charP == '.') {
-                indexS++;
-                indexP++;
-            } else if (charP == '*') {
-                if (charArrP[indexP - 1] != '.') {
-                    if (charArrP[indexP - 1] != charArrS[indexS - 1]) {
+        int m = s.length();
+        int n = p.length();
 
-                    } else if (charArrS[indexS - 1] != charS) {
-
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
                     }
                 }
-
-                if (charS == charArrS[indexS]) {
-                    indexS++;
-                }
-            } else {
-                return false;
             }
         }
-        return hasCompliant;
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
